dfff961036df4d5f53d78670fc986d32abf11314
Leonardo Naoki Higa <leonardonaoki@gmail.com>
2025-02-28 09:06:17 -0300

    Initial files

---
+++ src/main/java/com/acompanhamento/paciente/sus/acompanhamentopacientesus/AcompanhamentopacientesusApplication.java
@@ -0,0 +1,13 @@
+package com.acompanhamento.paciente.sus.acompanhamentopacientesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcompanhamentopacientesusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcompanhamentopacientesusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acompanhamento/paciente/sus/acompanhamentopacientesus/AcompanhamentopacientesusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acompanhamento.paciente.sus.acompanhamentopacientesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcompanhamentopacientesusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
