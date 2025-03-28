8e558dfca5af54f3acc755eacc494025c31aec6a
Leonardo Naoki Higa <leonardonaoki@gmail.com>
2025-03-28 15:44:04 -0300

    Fix main test
    TODO: Testes unitários.

--- src/test/java/com/acompanhamento/paciente/sus/acompanhamentopacientesus/AcompanhamentopacientesusApplicationTest.java
+++ src/test/java/com/acompanhamento/paciente/sus/acompanhamentopacientesus/AcompanhamentopacientesusApplicationTest.java
@@ -1,16 +1,15 @@
 package com.acompanhamento.paciente.sus.acompanhamentopacientesus;
 
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+
 @ActiveProfiles("test")
 class AcompanhamentopacientesusApplicationTest {
 
   @Test
   void mainMethodStartsApplication() {
+    System.setProperty("spring.profiles.active", "test");
     assertDoesNotThrow(() -> AcompanhamentopacientesusApplication.main(new String[] {}));
   }
 }
